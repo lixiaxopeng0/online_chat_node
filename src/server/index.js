606579ffa4aaa3edb2812ac87caf0b7cf1ffc0c4
starry <9738097+boundless-starry-sky@user.noreply.gitee.com>
2023-03-20 06:24:41 +0000

    update src/server/index.js. 文件读取修改
    Signed-off-by: starry <9738097+boundless-starry-sky@user.noreply.gitee.com>

--- src/server/index.js
+++ src/server/index.js
@@ -11,36 +11,45 @@ const io = require('socket.io')(http, {
         // credentials: true, // 允许带上 cookie
     }
 });
-// const fs = require('fs');
-
-http.listen(8006, () => {
-    console.log('http://192.168.0.104:8006, 服务器已开启...');
-});
+const fs = require('fs');
 
 let login_username = '';
-// const loginUserFile = 'login.txt';
-// let loginText = '';
+const loginUserFile = './login.txt';
+let loginText = '';
+http.listen(8006, () => {
+    console.log('http://192.168.0.104:8006, 服务器已开启...');
+    fs.readFile(loginUserFile, 'utf8', (err, data) => {
+        if (err) throw err;
+        loginText = data;
+    });
+});
+
 io.on('connection', (socket) => {
     console.log('连接成功...')
-    socket.on('message', (data) => {
+    socket.on('message', async (data) => {
         login_username = data.username;
-        // fs.readFile('file.txt', 'utf8', (err, data) => {
-        //     loginText = data;
-        //     if (err) throw err;
-        // });
-        // const isHasLogined = loginText?.split(',').includes(data.username);
-        // if (!isHasLogined) {
-        //     fs.appendFile(loginUserFile, `${data.username},`, (err) => {
-        //         if (err) throw err;
-        //         console.log('The data was appended to file!');
-        //     });
-        // }
-        io.emit('showUser', `"${data.username}"已加入聊天群！`);
+        const isHasLogined = loginText.includes(`,${login_username},`, loginText);
+        if (!isHasLogined) {
+            fs.appendFile(loginUserFile, `${login_username},`, (err) => {
+                if (err) throw err;
+                console.log('The data was appended to file!');
+            });
+            loginText = `${loginText}${login_username},`;
+        }
+        io.emit('showUser', {
+            text: `"${login_username}"已加入聊天群！`,
+            login_username,
+        });
     });
     socket.on('send', (data) => {
         io.emit('accepted', data);
     });
     socket.on('disconnect', () => {
         io.emit('leave', `"${login_username}"离开聊天室`);
+        loginText.replace(`,${login_username},`, ',');
+        fs.writeFile(loginUserFile, loginText, (err) => {
+            if (err) throw err;
+            console.log('The data was writed to file!');
+        });
     });
 });
